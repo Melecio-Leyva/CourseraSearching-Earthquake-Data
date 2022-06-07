57c29f4ce305a16fa6ebc0ec3d2ab8fba8658011
Melecio-Leyva <61437633+Melecio-Leyva@users.noreply.github.com>
2022-06-06 21:45:03 -0700

    Add files via upload

--- EarthQuakeClient.java
+++ EarthQuakeClient.java
@@ -18,12 +18,16 @@ public class EarthQuakeClient {
         return answer;
     }
 
-    public ArrayList<QuakeEntry> filterByDistanceFrom(ArrayList<QuakeEntry> quakeData,
-    double distMax,
-    Location from) {
+    public ArrayList<QuakeEntry> filterByDistanceFrom(ArrayList<QuakeEntry> quakeData, double distMax, Location from) {
         ArrayList<QuakeEntry> answer = new ArrayList<QuakeEntry>();
         // TODO
-
+        for(int i =0; i <quakeData.size();i++){
+            QuakeEntry quake = quakeData.get(i);            
+            double distanceInMeters = from.distanceTo(quake.getLocation())/1000;
+            if(distanceInMeters<distMax){
+                answer.add(quakeData.get(i));
+            }
+        }
         return answer;
     }
 
@@ -54,15 +58,22 @@ public class EarthQuakeClient {
 
     public void closeToMe(){
         EarthQuakeParser parser = new EarthQuakeParser();
-        String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        String source = "data/nov20quakedatasmall.atom";
         ArrayList<QuakeEntry> list  = parser.read(source);
         System.out.println("read data for "+list.size()+" quakes");
 
         // This location is Durham, NC
-        Location city = new Location(35.988, -78.907);
-
+        //Location city = new Location(35.988, -78.907);
         // This location is Bridgeport, CA
-        // Location city =  new Location(38.17, -118.82);
+         Location city =  new Location(38.17, -118.82);
+        ArrayList<QuakeEntry> temp  = filterByDistanceFrom(list,1000.00,city);
+        
+        for(int i =0;i<temp.size();i++){
+            QuakeEntry entry = temp.get(i);
+            double distanceInMeters = city.distanceTo(entry.getLocation())/1000;
+            System.out.println(distanceInMeters + " " +entry.getInfo());
+        }
+        System.out.println("read data for "+temp.size()+" quakes" + " meet the criteria");
 
         // TODO
     }
